8acc650fbeef9e6c7202102277074a35798fc531
705Pahlavon <pahlavondovranov@gmail.com>
2025-07-26 07:08:12 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+BOT_TOKEN = "7845322143:AAHeTRtguhXssoxoi0RhQTF1zpmpsjaB_nU"
+ADMIN_ID = 7824942822
+SUB_CHANNELS = ["@instaXsSaver"]
+OPENAI_API_KEY = "YOUR_OPENAI_API_KEY"  # keyinchalik bu joyni to‘ldirasiz
