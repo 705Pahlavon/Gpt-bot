95863a0ff118ab52c529808b8e0831d27ce2a4b2
705Pahlavon <pahlavondovranov@gmail.com>
2025-07-26 07:29:12 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 BOT_TOKEN = "7845322143:AAHeTRtguhXssoxoi0RhQTF1zpmpsjaB_nU"
 ADMIN_ID = 7824942822
 SUB_CHANNELS = ["@instaXsSaver"]
-OPENAI_API_KEY = "YOUR_OPENAI_API_KEY"  # keyinchalik bu joyni to‘ldirasiz
+OPENAI_API_KEY = "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"  # keyinchalik bu joyni to‘ldirasiz
